c3a7a81b567caab1b36cfa271eb665b542db2a15
Erik Bezerra Pereira da Silva <33065800+ErikPereira@users.noreply.github.com>
2019-10-19 17:39:38 -0300

    Problemas resolvidos
    Criptografia funcionando definitivamente

--- cryptoapi-demo.c
+++ cryptoapi-demo.c
@@ -34,6 +34,12 @@ module_param_string(keyPega,keyPega,17,0);
 static char ivPega[17];
 module_param_string(ivPega,ivPega,17,0);
 
+static char inputPega[17];
+module_param_string(inputPega,inputPega,17,0);
+
+static int escolha;
+module_param(escolha, int, 0);
+
 static char   key[16];
 static char   iv[16];
 
@@ -53,7 +59,8 @@ MODULE_LICENSE("GPL");
 static void hexdump(unsigned char *buf, unsigned int len)
 {
         while (len--){
-                printk(" %02x", *buf++);
+		//printk(" %02x", *buf++);
+		printk(" %c", *buf++);
 	}
         //printk("\n");
 }
@@ -64,28 +71,28 @@ static void cryptoapi_demo(void)
         char *algo = "cbc(aes)"; // modificado: anterior "aes"
         int   mode = CRYPTO_skcipher_MODE_CBC;
 	int   mask = CRYPTO_skcipher_MODE_MASK; // adicionado
-   	int i;	
+   	int  i;	
 
         /* local variables */
         struct crypto_skcipher *tfm; // utilzamos skcipher ao inves de tfm
-        struct scatterlist sg[4]; // entender o q eh scatterlist
+        struct scatterlist sg[2]; // entender o q eh scatterlist
         struct skcipher_request *req = NULL; // necessario apra criptografar
-        struct skcipher_request *reqTeste = NULL;
         int    ret;
-        char   *input, *output, *teste, *inputTeste,*saida1, *saida2;
+        char   *input, *output, *saida;
 	
-
+			
 	for(i=0; i < 16; i++){
-		if(keyPega[i])
-			key[i] = keyPega[i];
-		else
+		if(keyPega[i] == '\0')
 			key[i] = 0;
-	
-		if(ivPega[i])
-			iv[i] = ivPega[i];
 		else
+			key[i] =  keyPega[i];
+	
+		if(ivPega[i] == '\0')
 			iv[i] = 0;
-	}	
+		else
+			iv[i] = ivPega[i];
+
+	}
 	
 
         tfm = crypto_alloc_skcipher(algo, mode, mask);
@@ -108,26 +115,6 @@ static void cryptoapi_demo(void)
         	goto out;
     	}
 
-	
-	reqTeste = skcipher_request_alloc(tfm, GFP_KERNEL); // o que é GFP_KERNEL????
-    	if (!reqTeste) {
-        	printk("could not allocate tfm request\n");
-        	goto out;
-    	}
-	
-	teste = kmalloc(DATA_SIZE, GFP_KERNEL); // Parametros estavam invertidos
-        if (!teste) {
-                printk(KERN_ERR PFX "kmalloc(input) failed\n");
-                goto out;
-        }
-
-	inputTeste = kmalloc(DATA_SIZE, GFP_KERNEL); // Parametros estavam invertidos
-        if (!inputTeste) {
-                printk(KERN_ERR PFX "kmalloc(input) failed\n");
-                goto out;
-        }
-
-
         input = kmalloc(DATA_SIZE, GFP_KERNEL); // Parametros estavam invertidos
         if (!input) {
                 printk(KERN_ERR PFX "kmalloc(input) failed\n");
@@ -140,84 +127,82 @@ static void cryptoapi_demo(void)
                 kfree(input);
                 goto out;
         }
+	
+		
+	
+	for(i=0; i < 16; i++){
 
-        memset(input, 1, DATA_SIZE);
-	memset(inputTeste, 1, DATA_SIZE);
+		if(inputPega[i] == '\0')
+			input[i] = 0;
+		else
+			input[i] = inputPega[i];
+
+	};
 
         sg_init_one(&sg[0], input, DATA_SIZE);          //
-        sg_init_one(&sg[1], output, DATA_SIZE);      //      entrou no lugar do FILL_SG();
- 	sg_init_one(&sg[2], teste, DATA_SIZE);      //      entrou no lugar do FILL_SG();
- 	sg_init_one(&sg[3], inputTeste, DATA_SIZE);
+        sg_init_one(&sg[1], output, DATA_SIZE);      	//      entrou no lugar do FILL_SG();
 
-	printk(KERN_ERR PFX "IN 1: "); hexdump(input, DATA_SIZE);
-	printk(KERN_ERR PFX "EN 1: "); hexdump(output, DATA_SIZE);
-
-	//printk(KERN_ERR PFX "Teste 1: "); hexdump(teste, DATA_SIZE);	
-
-        //skcipher_request_set_crypt(reqTeste, &sg[3], &sg[2], DATA_SIZE, iv); // ordem ods parametros: requisição, origem, destino, tamanho, iv;
  	skcipher_request_set_crypt(req, &sg[0], &sg[1], DATA_SIZE, iv); // ordem ods parametros: requisição, origem, destino, tamanho, iv;
  	
-	//crypto_skcipher_encrypt(reqTeste);
 
-	crypto_skcipher_encrypt(req);	
-        
-	saida1 = sg_virt(&sg[1]);	
+	if(escolha == 1){
 
-	printk(KERN_ERR PFX "IN 2: "); hexdump(input, DATA_SIZE);
-	printk(KERN_ERR PFX "EN 2: "); hexdump(saida1, DATA_SIZE);
+		printk("Encrypt Correct:\n\n\n");
 
-	/*printk(KERN_ERR PFX "Teste 2: "); hexdump(teste, DATA_SIZE);
-	
-	strcpy(input,teste);
-	
-	if(input == NULL || teste == NULL){
-		goto out_kfree;
+		crypto_skcipher_encrypt(req);	
+		
+		saida = sg_virt(&sg[1]);	
+
+		printk(KERN_ERR PFX "Input: "); hexdump(input, DATA_SIZE);
+		printk(KERN_ERR PFX "Output: "); hexdump(saida, DATA_SIZE+1);
 	}
-
-	printk("Descrypt Teste:\n\n\n");
-
-	crypto_skcipher_decrypt(reqTeste);
-
-	printk(KERN_ERR PFX "IN 3: "); hexdump(input, DATA_SIZE);
-	printk(KERN_ERR PFX "Teste 3: "); hexdump(teste, DATA_SIZE);	
-        */
-
-
-	printk("Descrypt Certo:\n\n\n");
+	else{
+		printk("Descrypt Correct:\n\n\n");
+	 	
+		inputPega[0] = 223;
+		inputPega[1] = 70;
+		inputPega[2] = 92;
+		inputPega[3] = 216;
+		inputPega[4] = 61;
+		inputPega[5] = 248;
+		inputPega[6] = 4;
+		inputPega[7] = 166;
+		inputPega[8] = 28;
+		inputPega[9] = 108;
+		inputPega[10] = 12;
+		inputPega[11] = 196;
+		inputPega[12] = 127;
+		inputPega[13] = 218;
+		inputPega[14] = 103;
+		inputPega[15] = 148;
 	
-	strcpy(teste,saida1);
+			
+		for(i=0; i < 16; i++){
 
-	if(input == NULL || output == NULL){
-		goto out_kfree;
-	}
+		if(inputPega[i] == '\0')
+			input[i] = 0;
+		else
+			input[i] = inputPega[i];
 
-	skcipher_request_set_crypt(reqTeste, &sg[2], &sg[3], DATA_SIZE, iv); // ordem ods parametros: requisição, origem, destino, tamanho, iv;
- 	
-	crypto_skcipher_decrypt(reqTeste);
+		}
+
+		crypto_skcipher_decrypt(req);
 	
-	saida2 = sg_virt(&sg[3]);
+		saida = sg_virt(&sg[1]);
 	
-	printk(KERN_ERR PFX "IN 3: "); hexdump(teste, DATA_SIZE);
-	printk(KERN_ERR PFX "EN 3: "); hexdump(saida2, DATA_SIZE);	
-												
-	out_kfree:
+		printk(KERN_ERR PFX "Input: "); hexdump(input, DATA_SIZE);
+		printk(KERN_ERR PFX "Output: "); hexdump(saida, DATA_SIZE+1);	
+	}											
+	//out_kfree:
  
         kfree(output);
         kfree(input);
-	kfree(teste);
-	kfree(inputTeste);
 
 	out:
         crypto_free_skcipher(tfm);
         skcipher_request_free(req);
-	skcipher_request_free(reqTeste);
-
-
 	
-	
-		
-	
-	}
+   }
 
 /* ====== Module init/exit ====== */
 
